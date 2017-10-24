cd33b2189a21ef03505d5a9145c2b81df5695376
jinredhub <jin@nieblo.com>
2017-10-24 12:16:35 -0700

    changed exteded true

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ var PORT = process.env.PORT || 3000;
 app.use(express.static("public"));
 
 // Parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // handlebars
