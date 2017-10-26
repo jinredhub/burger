9cd2e766bb6db2b40a825147cae1f125fc1eacfe
jinredhub <jin@nieblo.com>
2017-10-26 00:09:01 -0700

    added heroku db setting

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,23 @@
 var mysql = require("mysql");
+var connection;
 
-// mysql
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "my456123",
-  database: "burgers_db"
-});
+if(process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+}
+else{
+	// mysql
+	  connection = mysql.createConnection({
+	  host: "localhost",
+	  user: "root",
+	  password: "my456123",
+	  database: "burgers_db"
+	});
+}
 
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
 });
+
 
 module.exports = connection;
